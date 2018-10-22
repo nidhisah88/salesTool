4bb34570cd4c58194ababcc408d14d4ed0b9d755
hp <hp@admin>
2018-10-23 01:42:30 +0530

    adding first change to index file

---
+++ REPOproject/src/index.java
@@ -0,0 +1,24 @@
+
+public class index {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		salesdata data = new salesdata();
+		displayGreeting();
+		data.display();
+		
+	}
+	
+
+	public static void displayGreeting() {
+		// TODO Auto-generated method stub
+		System.out.println("HAPY SALES");
+		System.out.println("this app shows sales data");
+		
+		System.out.println("test1 changing");
+		
+	}
+
+}
